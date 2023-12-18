0288427a732da8d58687fea1487ce32351338443
RokasV <vezikauskas.r@gmail.com>
2023-12-18 11:43:55 +0200

    people

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
+let people = require('./db/people');
 const app = express();
 
 const PORT = 3000;
-
 // DATA
 
 let users = [
@@ -32,11 +31,51 @@ let users = [
 // Middleware
 app.use(morgan('dev'));
 app.use(cors());
+app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+// PEOPLE ROUTES
+// GET = /api/people - get all
+app.get('/api/people', (req, res) => {
+  res.json(people);
+});
+app.get('/api/people/drivers', (req, res) => {
+  const drivers = people.filter((pObj) => pObj.hasCar);
+  res.json(drivers);
+});
+// GET - /apie/people/1 get single
+app.get('/api/people/:userId', (req, res) => {
+  const userId = +req.params.userId;
+  const found = people.find((obj) => obj.id === userId);
+  if (found === undefined) {
+    res.status(404).json({
+      msg: `user not found with id ${userId}`,
+    });
+    return;
+  }
+  res.json(found);
+});
+// DELETE - /apie/people/1 delete single
+app.delete('/api/people/:userId', (req, res) => {
+  const userId = +req.params.userId;
+  people = people.filter((uObj) => uObj.id !== userId);
+  res.json(people);
+});
+// PUT - /apie/people/1 update person
+app.put('/api/people/:userId', (req, res) => {
+  const userId = +req.params.userId;
+  const found = people.find((obj) => obj.id === userId);
+  const { sex, name } = req.body;
+  found.name = name;
+  found.sex = sex;
+  res.json(found);
+});
+// POST - /apie/people add new person
+app.post('apie/people');
 
 // GET /api/users - grazina visus vartotojus
 app.get('/api/users', (req, res) => {
@@ -60,6 +99,19 @@ app.delete('/api/users/:userId', (req, res) => {
   const userId = +req.params.userId;
   users = users.filter((uObj) => uObj.id !== userId);
   res.json(users);
+});
+
+// PUT /api/users/1 - nusiusti ka atnaujinti - grazinti atnaujinta objekta
+app.put('/api/users/:userId', (req, res) => {
+  console.log('req.body ===', req.body);
+  // surasti useri su od === userId
+  // atnaujinti jo savybes
+  const userId = +req.params.userId;
+  const found = users.find((obj) => obj.id === userId);
+  const { town, name } = req.body;
+  found.name = name;
+  found.town = town;
+  res.json(found);
 });
 
 // Run the server
